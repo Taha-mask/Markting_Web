8148079b180250d0bca48214da621ca2b89d5e1a
Husseini <hosinyahmed2@gmail.com>
2025-04-18 17:02:53 +0200

    Add about component files

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,12 +1,40 @@
 import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-about',
-  standalone: true,
-  imports: [],
-  templateUrl: './about.component.html',
-  styleUrl: './about.component.css'
-})
-export class AboutComponent {
-
-}
+ import { trigger, style, transition, animate, keyframes } from '@angular/animations';
+ 
+ 
+ @Component({
+   selector: 'app-about',
+   standalone: true,
+   imports: [],
+   templateUrl: './about.component.html',
+   styleUrl: './about.component.css',
+     animations: [
+     trigger('fadeInLeft', [
+       transition(':enter', [
+         style({ opacity: 0, transform: 'translateX(-50px)' }),
+         animate('1s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+       ])
+     ]),
+     trigger('fadeInRight', [
+       transition(':enter', [
+         style({ opacity: 0, transform: 'translateX(50px)' }),
+         animate('1s ease-out', style({ opacity: 1, transform: 'translateX(0)' }))
+       ])
+     ])
+   ]
+ })
+ 
+ export class AboutComponent {
+ showMoreText: boolean = false;
+ 
+   toggleText(): void {
+     this.showMoreText = !this.showMoreText;
+   }
+ 
+   scrollToSection() {
+     const element = document.getElementById('todown');
+     if (element) {
+       element.scrollIntoView({ behavior: 'smooth' });
+     }
+   } 
+ }
