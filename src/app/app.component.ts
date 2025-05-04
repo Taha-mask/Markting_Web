2379a4b8f70b80eeeea1d06d7861a282eb36e3a5
Husseini <hosinyahmed2@gmail.com>
2025-05-04 20:51:10 +0300

    resolove some issues in story

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,10 +7,11 @@ import {SavedPostComponent} from './components/saved-post/saved-post.component';
 import { PostDetailsComponent } from './components/post-details/post-details.component';
 import { InnerStoryComponent } from "./components/inner-story/inner-story.component";
 import { ProfileAsVisitorComponent } from './components/profile-as-visitor/profile-as-visitor.component';
+import { RouterModule } from '@angular/router';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent, PostDetailsComponent, InnerStoryComponent, ProfileAsVisitorComponent],
+  imports: [RouterModule,CommonModule, NavbarComponent, FooterComponent, PostDetailsComponent, InnerStoryComponent, ProfileAsVisitorComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
